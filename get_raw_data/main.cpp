2e7230932821322c26af14a3dcd9d9442676740d
Dinesh Atchuthan <datchuth@laas.fr>
2016-08-02 06:30:35 +0200

    comment

--- get_raw_data/main.cpp
+++ get_raw_data/main.cpp
@@ -26,6 +26,10 @@
     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+/*
+last modified
+Author : Dinesh Atchuthan
+*/
 
 #include <xsens/xsportinfoarray.h>
 #include <xsens/xsdatapacket.h>
