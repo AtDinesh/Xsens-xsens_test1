844242c78c3231838dd43714ce9d67f9823ff3f4
Dinesh Atchuthan <datchuth@laas.fr>
2016-08-02 11:47:12 +0200

    changes in timestamp

--- get_HR_data/main.cpp
+++ get_HR_data/main.cpp
@@ -469,7 +469,7 @@ int main(int argc, char* argv[])
                         imu_publi.publish(imu_msg);
                     }
                     else{
-                        timestamp = ros::Time::now();
+                        imu_msg.header.stamp = ros::Time::now();
                         imu_publi.publish(imu_msg);
                     }
 
